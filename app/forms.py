b4571f3b37e1c400cc8f59da639b0d53f7207274
randomdudefrombobsuniverse <kirimimathiu2020@gmail.com>
2024-11-22 20:13:10 +0300

    first commit

---
+++ app/forms.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, TextAreaField, SelectField
+from wtforms.validators import DataRequired, Length, Email
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Edit Profile')
+
+class LoginForm(FlaskForm):
+    """Login Form"""
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep Me Logged In')
+    submit = SubmitField('Log in')
+
+class RegisterForm(FlaskForm):
+    """Register Form"""
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64)])
+    email = StringField('Email',validators=[DataRequired(), Length(1, 64),Email()] )
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+class AddProductForm(FlaskForm):
+    """AddProduct Form"""
+    product_name = StringField('Product Name', validators=[DataRequired(), Length(1, 64)])
+    product_description = TextAreaField('Product Description',validators=[DataRequired(), Length(1, 64),Email()] )
+    stock_available = SelectField('Stock Available', 
+    choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5') ], validators=[DataRequired()])
+    submit = SubmitField('AddProduct')
+
+class PostForm(FlaskForm):
+    """Comment Form"""
+    body = TextAreaField('Body', validators=[DataRequired()])
+    submit = SubmitField('Post')
